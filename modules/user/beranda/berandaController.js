cd9ce43ade11a2d310da7f6e2126f148cac460db
Soumindar Qolby <soumindar@Soumindars-MacBook-Air.local>
2022-12-26 23:37:22 +0700

    Update homepage content

--- modules/user/beranda/berandaController.js
+++ modules/user/beranda/berandaController.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 const getBaseUrl = require('../../../utils/getBaseUrl');
 const sessionVerify = require('../auth/sessionVerify');
+const berandaService = require('./berandaService');
 
 router.use(sessionVerify);
 
 // home page user
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const baseUrl = getBaseUrl(req);
+  const pegawai = await berandaService.dataBeranda(req, res);
+  
   return res.render('user/beranda/beranda', {
     baseUrl,
     req,
+    data: pegawai.data,
   });
 });
 
---
+++ modules/user/beranda/berandaService.js
@@ -0,0 +1,47 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const getBaseUrl = require('../../../utils/getBaseUrl');
+const momentTz = require('moment-timezone');
+const userTimezone = require('../../../config/timezone.config');
+
+// data pegawai di beranda
+const dataBeranda = async (req, res) => {
+  try {
+    const idPegawai = req.session.idPegawai;
+    const pegawai = await prisma.pegawai.findFirst({
+      select: {
+        nip: true,
+        nama: true,
+        tglLahir: true,
+        jabatan: true,
+        divisi: true,
+      },
+      where: { id: idPegawai },
+    });
+
+    if (!pegawai) {
+      req.session.error = [{ msg: 'User tidak ditemukan' }];
+      return {
+        statusCode: 404,
+      };
+    }
+
+    pegawai.tglLahir = pegawai.tglLahir.getFullYear() + "-" + ("0"+(pegawai.tglLahir.getMonth()+1)).slice(-2) + "-" + ("0" + pegawai.tglLahir.getDate()).slice(-2);
+    
+    return {
+      statusCode: 200,
+      data: pegawai,
+    };
+  } catch (error) {
+    const baseUrl = getBaseUrl(req);
+    return res.render('user/error', {
+      baseUrl,
+      req,
+      statusCode: 500,
+    });
+  }
+};
+
+module.exports = {
+  dataBeranda,
+}
