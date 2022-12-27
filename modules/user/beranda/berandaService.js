cb7246eaf9b453f5f05b0af38e8e66c6b12f6785
Soumindar Qolby <soumindar@Soumindars-MacBook-Air.local>
2022-12-27 16:41:13 +0700

    Add statistics on homepage

--- modules/user/beranda/berandaService.js
+++ modules/user/beranda/berandaService.js
@@ -1,8 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const getBaseUrl = require('../../../utils/getBaseUrl');
-const momentTz = require('moment-timezone');
-const userTimezone = require('../../../config/timezone.config');
+const toDateObj = require('../../../utils/toDateObj');
 
 // data pegawai di beranda
 const dataBeranda = async (req, res) => {
@@ -31,11 +30,143 @@ const dataBeranda = async (req, res) => {
     pegawai.tglLahir = pegawai.tglLahir.getFullYear() + "-" + ("0"+(pegawai.tglLahir.getMonth()+1)).slice(-2) + "-" + ("0" + pegawai.tglLahir.getDate()).slice(-2);
     pegawai.foto = (!pegawai.foto) ? `${baseUrl}/img/user/no_avatar.jpeg` : `${baseUrl}/img/user/${idPegawai}/${pegawai.foto}`;
 
+    const today = toDateObj(new Date());
+    const akhirBulan = toDateObj(new Date(today.getFullYear(), today.getMonth()+1, 0));
+    const awalbulan = toDateObj(new Date(today.getFullYear(), today.getMonth(), 1));
+    const awalTahun = toDateObj(new Date(today.getFullYear(), 0, 1));
+    const akhirTahun = toDateObj(new Date(today.getFullYear(), 11, 31));
+
+    const getAktivitasBulanIni = await prisma.aktivitasPegawai.aggregate({
+      _count: { id: true },
+      where: {
+        idPegawai,
+        tglMulai: {
+          lte: akhirBulan,
+          gte: awalTahun,
+        },
+        tglSelesai: {
+          gte: awalbulan,
+          lte: akhirTahun,
+        },
+      }
+    });
+
+    const getAktivitasTahunIni = await prisma.aktivitasPegawai.aggregate({
+      _count: { id: true },
+      where: {
+        idPegawai,
+        tglMulai: {
+          gte: awalTahun,
+          lte: akhirTahun,
+        },
+        tglSelesai: {
+          gte: awalTahun,
+          lte: akhirTahun,
+        },
+      }
+    });
+
+    const aktivitasBulanIni = getAktivitasBulanIni._count.id;
+    const aktivitasTahunIni = getAktivitasTahunIni._count.id;
+
+    const aktivitasSelesai = await prisma.aktivitasPegawai.findMany({
+      select: {
+        id: true,
+        tglMulai: true,
+        tglSelesai: true,
+        realisasi: true,
+        pekerjaan: {
+          select: {
+            target: true,
+            level: {
+              select: { pengali: true },
+            },
+          },
+        },
+      },
+      where: {
+        idPegawai,
+        tglMulai: {
+          gte: awalTahun,
+          lte: today,
+        },
+        tglSelesai: {
+          gte: awalTahun,
+          lte: today,
+        }
+      }
+    });
+
+    let totalCkp = 0;
+    let maxCkp = 0;
+    let realisasiKosong = 0;
+    aktivitasSelesai.forEach(aktivitas => {
+      const nilaiAktivitas = (aktivitas.realisasi / aktivitas.pekerjaan.target) * aktivitas.pekerjaan.level.pengali;
+      totalCkp += nilaiAktivitas;
+      maxCkp += aktivitas.pekerjaan.level.pengali;
+
+      if (aktivitas.realisasi == 0) {
+        realisasiKosong++;
+      }
+    });
+    const progressCkp = Math.floor((totalCkp / maxCkp) * 100);
+
+    const awalTahunLalu = toDateObj(new Date(today.getFullYear() - 1, 0, 1));
+    const akhirTahunLalu = toDateObj(new Date(today.getFullYear() - 1, 11, 31));
+    const aktivitasTahunLalu = await prisma.aktivitasPegawai.findMany({
+      select: {
+        id: true,
+        tglMulai: true,
+        tglSelesai: true,
+        realisasi: true,
+        pekerjaan: {
+          select: {
+            target: true,
+            level: {
+              select: { pengali: true },
+            },
+          },
+        },
+      },
+      where: {
+        idPegawai,
+        tglMulai: {
+          gte: awalTahunLalu,
+          lte: akhirTahunLalu,
+        },
+        tglSelesai: {
+          gte: awalTahunLalu,
+          lte: akhirTahunLalu,
+        },
+      },
+    });
+
+    let totalCkpSebelumnya = 0;
+    let maxCkpSebelumnya = 0;
+    if (aktivitasTahunLalu.length > 0) {
+      aktivitasTahunLalu.forEach(aktivitas => {
+        const nilaiAktivitas = (aktivitas.realisasi / aktivitas.pekerjaan.target) * aktivitas.pekerjaan.level.pengali;
+        totalCkpSebelumnya += nilaiAktivitas;
+        maxCkpSebelumnya += aktivitas.pekerjaan.level.pengali;
+      });
+    }
+    
+    let ckpTahunLalu = 0;
+    if (maxCkpSebelumnya > 0) {
+      ckpTahunLalu = Math.floor((totalCkpSebelumnya / maxCkpSebelumnya) * 100);
+    }
+    
     return {
       statusCode: 200,
-      data: pegawai,
+      pegawai,
+      aktivitasBulanIni,
+      aktivitasTahunIni,
+      progressCkp,
+      realisasiKosong,
+      ckpTahunLalu,
     };
   } catch (error) {
+    console.log(error.message);
     const baseUrl = getBaseUrl(req);
     return res.render('user/error', {
       baseUrl,
