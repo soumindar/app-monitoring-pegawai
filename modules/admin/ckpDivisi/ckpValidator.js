328460dcba87dc86ffcbff83884b202d1c18f44c
Soumindar Qolby <soumindar@Soumindars-MacBook-Air.local>
2022-12-30 05:14:00 +0700

    Update validator

--- modules/admin/ckpDivisi/ckpValidator.js
+++ modules/admin/ckpDivisi/ckpValidator.js
@@ -3,6 +3,7 @@ const getBaseUrl = require('../../../utils/getBaseUrl');
 
 // validator tambah ckp
 const tambahCkp = [
+  check('idDivisi').notEmpty().withMessage('ID divisi tidak boleh kosong!'),
   check('tahun').notEmpty().withMessage('Tahun tidak boleh kosong!'),
   check('tahun').isInt({min: 2000, max: 2100}).withMessage('Tahun harus berupa bilangan bulat antara 2000-2100!'),
   (req, res, next) => {
